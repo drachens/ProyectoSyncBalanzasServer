3bb7c846fad050bf00744b523b8f00d8b24998b9
drachens <drachen_665@hotmail.com>
2024-09-25 17:33:38 -0300

    Se agrego CORS en codigo servidor

--- serverHttp.py
+++ serverHttp.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, redirect, url_for
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 UPLOAD_FOLDER = "/usr/lib/balance-app-mainland/.dart_tool/productImages"
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -11,12 +12,12 @@ app.config['MAX_CONTENT_LENGTH'] = 1024*1024
 @app.route('/upload',methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return "No se ha mandado un archivo",400
+        return "No se ha mandado un archivo",401
     
     file = request.files['file']
 
     if file.filename == '':
-        return "No se selecciono ningun archivo", 400
+        return "No se selecciono ningun archivo", 402
     
     if file:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
@@ -24,4 +25,4 @@ def upload_file():
         return f'Archivo cargado exitosamente en {filepath}', 200 
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000,debug=True)
