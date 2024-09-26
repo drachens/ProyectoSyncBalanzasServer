e01102068e24eeb57323e909121c37ada7039a28
drachens <drachen_665@hotmail.com>
2024-09-26 16:47:32 -0300

    Se arregla sintaxis setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-REPO_URL = "https://github.com/drachens/ProyectoSyncBalanzasServer.git"
-CLONE_DIR = "/home/hprt/Escritorio/serverHttp/"
-PROGRAM_NAME = "serverHttp.py"
-SETUP_FILENAME = "requirements.txt"
-PROGRAM_DIR = "ProyectoSyncBalanzasServer"
+REPO_URL="https://github.com/drachens/ProyectoSyncBalanzasServer.git"
+CLONE_DIR="/home/hprt/Escritorio/serverHttp/"
+PROGRAM_NAME="serverHttp.py"
+SETUP_FILENAME="requirements.txt"
+PROGRAM_DIR="ProyectoSyncBalanzasServer"
 
 sudo apt-get update
 
 sudo apt-get install -y git nginx python3 python3-pip
 
-if [-d "$CLONE_DIR $PROGRAM_DIR"]; then
+if [ -d "$CLONE_DIR $PROGRAM_DIR" ]; then
     echo "La carpeta $PROGRAM_DIR ya existe. Eliminandola..."
     rm -rf "$CLONE_DIR $PROGRAM_DIR"
 fi 
@@ -21,7 +21,7 @@ cd "$CLONE_DIR"
 
 git clone -b server "$REPO_URL" 
 
-cd "$PROGRAM_DIR" || {echo "No se pudo acceder al directorio."; exit 1; }
+cd "$PROGRAM_DIR" || { echo "No se pudo acceder al directorio."; exit 1; }
 
 pip3 install -r "$SETUP_FILENAME"
 
